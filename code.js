d458c00d9031561a6ed69cc694439ec6aaff3422
Johan Bové <johan@johanbove.info>
2020-01-26 18:10:46 +0100

    Trying checkCharging
    Checking why the screen turns black after checking charging

--- code.js
+++ code.js
@@ -7,12 +7,13 @@ require("Storage").write("-jbb",`
 let chargingBlinker;
 
 function draw(level) {
+  console.log('draw', level);
   // Clear the screen
   g.clear();
   g.setFontAlign(0,0); // center font
   g.setFont("6x8",8); // bitmap font, 8x magnified
   // draw the current counter value
-  g.drawString(level + '%', g.getWidth()/2, g.getHeight()/2);
+  g.drawString(level.toString() + '%', g.getWidth()/2, g.getHeight()/2);
 }
 
 function getBatteryLevel() {
@@ -21,10 +22,8 @@ function getBatteryLevel() {
 
   draw(level);
 
-  checkChargingfunction(Bangle.isCharging(), level);
-
   // again, 10 secs later
-  setTimeout(getBatteryLevel, 10000);
+  setTimeout(getBatteryLevel, 10E3);
 }
 
 function blinkGreen() {
@@ -35,23 +34,26 @@ function blinkGreen() {
   }, 500);
 }
 
-function checkChargingfunction(charging, level) {
-  console.log('checkChargingfunction', charging, level, chargingBlinker);
-  if (chargingBlinker) {
-    clearInterval(chargingBlinker);
-  }
-  if (charging) {
-    chargingBlinker = blinkGreen();
-  }
+function checkChargingfunction(charging) {
+  console.log('checkChargingfunction', charging);
+  LED2.write(charging);
 }
 
 function main() {
+  console.log('starting jbb version 0.0.1');
   getBatteryLevel();
 }
 
 Bangle.on('charging', function(isCharging) {
-  checkChargingfunction(isCharging, E.getBattery());
+  getBatteryLevel();
 });
 
+g.clear();
+Bangle.loadWidgets();
+Bangle.drawWidgets();
+
 main();
+
+// Show launcher when middle button pressed
+setWatch(Bangle.showLauncher, BTN2, {repeat:false,edge:"falling"});
 `);
