b987ccc3d3d237cccffac8b5ebceaa378567aa35
Johan Bové <me@johanbove.info>
2020-02-05 08:49:32 +0100

    Reworked according to app dev standards

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# espruino-jbb
-Bangle.js battery level checker
+# Espruino Bangle.js "Battery Power"
+
+Bangle.js app showing battery level and charging status.
 
 Following [the "developing your own app" tutorial](https://github.com/espruino/BangleApps#developing-your-own-app) by Gordon Williams
+
+<https://blog.johanbove.info/BangleApps/>
+
+<https://www.espruino.com/Bangle.js>
--- deprecated.js
+++ deprecated.js
@@ -4,45 +4,57 @@ require("Storage").write("+jbb",{
 });
 
 require("Storage").write("-jbb",`
-let chargingBlinker;
+/**
+ * draw the current level value
+ */
 function draw(level) {
   console.log('draw', level);
   // Clear the screen
   g.clear();
   g.setFontAlign(0,0); // center font
   g.setFont("6x8",8); // bitmap font, 8x magnified
-  // draw the current counter value
-  g.drawString(level.toString() + '%', g.getWidth()/2, g.getHeight()/2);
+  g.drawString(level+"%",120,80);
+  g.setFont("6x8",4);
+  g.drawString("power",120,130);
 }
+
 function getBatteryLevel() {
   level = E.getBattery();
   console.log('getBatteryLevel', level);
+
   draw(level);
+
+  checkCharging(Bangle.isCharging());
+
   // again, 10 secs later
   setTimeout(getBatteryLevel, 10E3);
 }
-function blinkGreen() {
-  let on = false;
-  return setInterval(function() {
-    on = !on;
-    LED2.write(on);
-  }, 500);
+
+function checkCharging(charging) {
+  console.log('checkCharging', charging);
+  // Green LED
+  //LED2.write(charging);
+  if (charging) {
+    g.setFontAlign(0,0); // center font
+    g.setFont("6x8",3); // bitmap font, 3x magnifier
+    g.drawString("charging",120,160);
+  }
 }
-function checkChargingfunction(charging) {
-  console.log('checkChargingfunction', charging);
-  LED2.write(charging);
-}
+
 function main() {
   console.log('starting jbb version 0.0.1');
   getBatteryLevel();
 }
-Bangle.on('charging', function(isCharging) {
-  getBatteryLevel();
-});
+
 g.clear();
+g.flip();
 Bangle.loadWidgets();
 Bangle.drawWidgets();
+
+Bangle.on('charging', checkCharging);
+
 main();
+
 // Show launcher when middle button pressed
 setWatch(Bangle.showLauncher, BTN2, {repeat:false,edge:"falling"});
 `);
